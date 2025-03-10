6f7cf5c2186609ab5f945079133cc47b4301c127
namit53 <namitrampal53@gmail.com>
2025-03-10 16:24:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from battery_gui import BatteryMonitorApp
-from battery_refresh import update_battery_status
+from battery_status import update_battery_status
+from battery_refresh import update_battery_refresh
 
 root = tk.Tk()
 app = BatteryMonitorApp(root)
